34283c862b9e2fcd921e807db72a295b24dc2fd6
takumuka <takumuka@C010423125.zipangu.cac.co.jp>
2026-04-16 10:52:31 +0900

    クエスチョン１終わりました

--- java_comprehension_exercises_volume1/src/basic/question01/Question01_1.java
+++ java_comprehension_exercises_volume1/src/basic/question01/Question01_1.java
@@ -7,6 +7,15 @@ package basic.question01;
 public class Question01_1 {
 
 	public static void main(String[] args) {
+		int prise = 120;
+		String item = "ノート";
+		System.out.println(prise);
+		System.out.println(item);
+
+		prise = 200;
+		item = "鉛筆";
+		System.out.println(prise);
+		System.out.println(item);
 
 		// TODO:ここに実装
 
