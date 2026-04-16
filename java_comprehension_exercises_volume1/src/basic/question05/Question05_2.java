e22e6bc68d60f71dd47c5e78ba01f47626f528e3
takumuka <takumuka@C010423125.zipangu.cac.co.jp>
2026-04-16 13:51:03 +0900

    question5完了

--- java_comprehension_exercises_volume1/src/basic/question05/Question05_2.java
+++ java_comprehension_exercises_volume1/src/basic/question05/Question05_2.java
@@ -1,6 +1,8 @@
 package basic.question05;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第8章 条件分岐
@@ -9,6 +11,24 @@ import java.io.IOException;
 public class Question05_2 {
 
 	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int[] numberGroup = new int[4];
+		numberGroup[0] = 3;
+		numberGroup[1] = 5;
+		numberGroup[2] = 7;
+		numberGroup[3] = 9;
+
+		if (numberGroup[3] == 3) {
+			System.out.println("4番目の要素は３です。");
+		} else if (numberGroup[3] == 5) {
+			System.out.println("4番目の要素は5です。");
+		}
+		if (numberGroup[3] == 7) {
+			System.out.println("4番目の要素は7です。");
+		} else {
+			System.out.println("4番目の要素の値は、3、5、7以外の数値です。");
+
+		}
 
 		// TODO:ここに実装
 
--- java_comprehension_exercises_volume1/src/basic/question05/Question05_4.java
+++ java_comprehension_exercises_volume1/src/basic/question05/Question05_4.java
@@ -1,6 +1,8 @@
 package basic.question05;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第8章 条件分岐
@@ -9,6 +11,21 @@ import java.io.IOException;
 public class Question05_4 {
 
 	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("数学の試験結果の点数を入力してください。");
+		String strsugaku = reader.readLine();
+		int math = Integer.parseInt(strsugaku);
+		System.out.println("国語の試験の点数を入力してください。");
+		String strkokugo = reader.readLine();
+		int japanese = Integer.parseInt(strkokugo);
+		if (math >= 60 && japanese >= 60) {
+			System.out.println("数学と国語ともに合格です。");
+		} else if (math >= 60 || japanese >= 60) {
+			System.out.println("数学、国語どちらかが合格です。");
+		} else {
+			System.out.println("どちらの教科も不合格です");
+
+		}
 
 		// TODO:ここに実装
 
